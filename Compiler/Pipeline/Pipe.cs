33f1c2b4559af0b777d7ea3587bd9309332e74c0
Alex <afiesseler@gmail.com>
2022-05-26 21:09:51 +0200

    change return value of fallback method to bool change return value of finalize method to list add length field

--- Compiler/Pipeline/Pipe.cs
+++ Compiler/Pipeline/Pipe.cs
@@ -29,9 +29,13 @@ public abstract class Pipe<T>
         public override To ReadNext() => step.Execute(pipe)!;
 
         public override bool HasNext => pipe.HasNext;
+
+        public override long Length => pipe.Length;
     }
 
     public abstract long Position { get; set; }
+
+    public abstract long Length { get; }
 
     public abstract T ReadNext();
 
@@ -39,9 +43,13 @@ public abstract class Pipe<T>
 
     public Pipe<T2> Step<T2>(IPipeStep<T, T2> step) where T2 : class => new SteppedPipe<T, T2>(this, step);
 
-    public IEnumerable<T?> Finalize()
+    public List<T?> Finalize()
     {
+        var oldPosition = Position;
+        Position = 0;
+
         T? current;
+        List<T?> result = new();
 
         while (true)
         {
@@ -49,27 +57,32 @@ public abstract class Pipe<T>
 
             if (current is null)
             {
-                yield return HasNext
+                result.Add(HasNext
                     ? throw new InvalidDataException("Unreaded data available.")
-                    : default;
+                    : default);
 
                 break;
             }
 
-            yield return current;
+            result.Add(current);
         }
 
-        Position = 0;
+        Position = oldPosition;
+
+        return result;
     }
 
-    public void Fallback(Func<bool> action)
+    public bool Fallback(Func<bool> action)
     {
         var fallback = Position;
 
         if (!action())
         {
             Position = fallback;
+            return false;
         }
+
+        return true;
     }
 
     public T2? Expect<T2>() where T2 : T
