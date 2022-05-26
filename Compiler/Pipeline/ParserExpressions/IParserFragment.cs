872eb3ea86306ede8b54a59fc677fcf382e18d9b
Alex <afiesseler@gmail.com>
2022-05-26 21:05:50 +0200

    remove old ParserGenerator files

--- Compiler/Pipeline/ParserExpressions/IParserFragment.cs
+++
@@ -1,9 +0,0 @@
-﻿using Fornax.Compiler.Pipeline.ParserExpressions.Interfaces;
-using System;
-
-namespace Fornax.Compiler.Pipeline.ParserExpressions;
-
-public interface IParserFragment<TParserType>
-{
-    IParserExpressionToCount<T, TParserType> Expect<T>() where T : TParserType;
-}
--- Compiler/Pipeline/ParserExpressions/Interfaces/IParserExpressionToFilter.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Fornax.Compiler.Pipeline.ParserExpressions.Interfaces;
-
-public interface IParserExpressionToFilter<T, TParserType> : IParserExpressionToFinish<T, TParserType> where T : TParserType
-{
-    IParserExpressionToFilter<T, TParserType> Where(Func<T, bool> condition);
-}
