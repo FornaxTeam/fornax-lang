b637d2a88d78242eb8b74a16d8356bd176107ca0
Alex <afiesseler@gmail.com>
2022-06-04 13:25:01 +0200

    implement KeywordToken

---
+++ Compiler/Pipeline/Tokenizer/Tokens/Keywords/KeywordToken.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Fornax.Compiler.Pipeline.Tokenizer.Tokens.Keywords;
+
+public class KeywordToken : Token
+{
+    public KeywordType Type { get; private set; }
+
+    protected override bool Read(Pipe<char?> pipe)
+    {
+        foreach (var keyword in Enum.GetValues<KeywordType>())
+        {
+            if (pipe.Fallback(fallbackPosition =>
+            {
+                var keywordString = keyword.ToString().ToLower();
+
+                for (var i = 0; i < keywordString.Length; i++)
+                {
+                    if (!pipe.HasNext || pipe.ReadNext()!.Value != keywordString[i])
+                    {
+                        return false;
+                    }
+                }
+
+                return true;
+            }))
+            {
+                Type = keyword;
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ Compiler/Pipeline/Tokenizer/Tokens/Keywords/KeywordType.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fornax.Compiler.Pipeline.Tokenizer.Tokens.Keywords;
+
+public enum KeywordType
+{
+    Import,
+    Export,
+
+    Struct,
+    Interface,
+    Implement,
+    Enum,
+
+    Const,
+    Final,
+    Var,
+
+    Return,
+    Break,
+    Continue,
+
+    If,
+    While,
+    For,
+    Match,
+
+    As,
+    Is,
+}
