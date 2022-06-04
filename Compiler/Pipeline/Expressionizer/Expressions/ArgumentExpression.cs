26ec367272a1f2fde14282839588a638fbffbfe2
Alex <afiesseler@gmail.com>
2022-06-04 13:22:12 +0200

    fix ArgumentExpression

--- Compiler/Pipeline/Expressionizer/Expressions/ArgumentExpression.cs
+++ Compiler/Pipeline/Expressionizer/Expressions/ArgumentExpression.cs
@@ -1,27 +1,30 @@
+using Fornax.Compiler.Logging;
 using Fornax.Compiler.ParserGenerator;
 using Fornax.Compiler.Pipeline.Tokenizer;
 using Fornax.Compiler.Pipeline.Tokenizer.Tokens;
 
 namespace Fornax.Compiler.Pipeline.Expressionizer.Expressions;
 
-public record ArgumentExpression(long Start, long End, string Type, string Name) : Expression(Start, End)
+public record ArgumentExpression(long Start, long End, IdentifierToken? Type, IdentifierToken? Name) : Expression(Start, End)
 {
-    public static ArgumentExpression Read(Pipe<Token> pipe, WriteLog log)
+    public override string ToString() => base.ToString();
+
+    public static ArgumentExpression Read(Pipe<Token> pipe, WriteLog? log)
     {
         var start = pipe.Position;
-        var type = "";
-        var name = "";
+        IdentifierToken? type = null;
+        IdentifierToken? name = null;
 
         if (pipe.Fallback(fallbackPosition =>
         {
             return ParserFragment.Create()
                 .Expect<IdentifierToken>()
-                .Handle(token => type = token.Value)
+                .Handle(token => type = token)
                     .MessageIfMissing("Type expected.")
                 .Expect<WhitespaceToken>()
                     .MessageIfMissing("Whitespace expected.")
                 .Expect<IdentifierToken>()
-                    .Handle(token => name = token.Value)
+                    .Handle(token => name = token)
                     .MessageIfMissing("Parameter name expected.")
                 .Parse(pipe, null);
         }))
@@ -31,10 +34,10 @@ public record ArgumentExpression(long Start, long End, string Type, string Name)
 
         ParserFragment.Create()
             .Expect<IdentifierToken>()
-                .Handle(token => name = token.Value)
+                .Handle(token => name = token)
                 .MessageIfMissing("Parameter name expected.")
             .Parse(pipe, log);
 
-        return new(start, pipe.Position, "", name);
+        return new(start, pipe.Position, null, name);
     }
 }
